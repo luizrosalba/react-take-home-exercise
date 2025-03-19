65579578164887e479b0eeaefde12692fdbaf46b
luiz rosalba <luizrosalba@gmail.com>
2025-03-19 17:54:35 -0300

    Updates

---
+++ Task Manager_files/main.tsx
@@ -0,0 +1,16 @@
+import { jsxDEV } from "/react-take-home-exercise/@id/__x00__react/jsx-dev-runtime";
+import __vite__cjsImport1_react from "/react-take-home-exercise/node_modules/.vite/deps/react.js?v=9e963012"; const React = __vite__cjsImport1_react.__esModule ? __vite__cjsImport1_react.default : __vite__cjsImport1_react;
+import __vite__cjsImport2_reactDom_client from "/react-take-home-exercise/node_modules/.vite/deps/react-dom_client.js?v=9e963012"; const ReactDOM = __vite__cjsImport2_reactDom_client.__esModule ? __vite__cjsImport2_reactDom_client.default : __vite__cjsImport2_reactDom_client;
+import "/react-take-home-exercise/src/index.css?t=1742417343748";
+import App from "/react-take-home-exercise/src/App.tsx?t=1742417343748";
+ReactDOM.createRoot(document.getElementById("root")).render(/* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+  fileName: "/home/luiz/code/react-take-home-exercise/src/main.tsx",
+  lineNumber: 6,
+  columnNumber: 5
+}, this) }, void 0, false, {
+  fileName: "/home/luiz/code/react-take-home-exercise/src/main.tsx",
+  lineNumber: 5,
+  columnNumber: 62
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBU0k7QUFUSixPQUFPQSxXQUFXO0FBQ2xCLE9BQU9DLGNBQWM7QUFFckIsT0FBTztBQUVQLE9BQU9DLFNBQVM7QUFFaEJELFNBQVNFLFdBQVdDLFNBQVNDLGVBQWUsTUFBTSxDQUFFLEVBQUVDLE9BQ3BELHVCQUFDLE1BQU0sWUFBTixFQUNDLGlDQUFDLFNBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUFJLEtBRE47QUFBQTtBQUFBO0FBQUE7QUFBQSxPQUVBLENBQ0YiLCJuYW1lcyI6WyJSZWFjdCIsIlJlYWN0RE9NIiwiQXBwIiwiY3JlYXRlUm9vdCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSBcInJlYWN0XCI7XG5pbXBvcnQgUmVhY3RET00gZnJvbSBcInJlYWN0LWRvbS9jbGllbnRcIjtcblxuaW1wb3J0IFwiLi9pbmRleC5jc3NcIjtcblxuaW1wb3J0IEFwcCBmcm9tIFwiLi9BcHBcIjtcblxuUmVhY3RET00uY3JlYXRlUm9vdChkb2N1bWVudC5nZXRFbGVtZW50QnlJZChcInJvb3RcIikhKS5yZW5kZXIoXG4gIDxSZWFjdC5TdHJpY3RNb2RlPlxuICAgIDxBcHAgLz5cbiAgPC9SZWFjdC5TdHJpY3RNb2RlPlxuKTtcbiJdLCJmaWxlIjoiL2hvbWUvbHVpei9jb2RlL3JlYWN0LXRha2UtaG9tZS1leGVyY2lzZS9zcmMvbWFpbi50c3gifQ==
---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-take-home-exercise/sw.js', { scope: '/react-take-home-exercise/' })})}
