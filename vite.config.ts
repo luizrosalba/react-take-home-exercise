26c8ac7a43784399b63d55ac1ba44512c6e0e99e
luiz rosalba <luizrosalba@gmail.com>
2025-03-19 17:22:08 -0300

    pwa vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: "/react-take-home-exercise/",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+        type: 'module',
+      },
+      workbox: { clientsClaim: true, skipWaiting: true },
+      manifest: {
+        // Your manifest configuration here
+        name: 'Task Manager',
+        short_name: 'Tasks',
+        description: 'A great task manager',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+
+        start_url: '/',
+        icons: [
+          {
+            src: '/react-take-home-exercise/icon192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/react-take-home-exercise/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
 });
